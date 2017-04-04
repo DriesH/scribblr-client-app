b95a5fe4b67bea5b2147bea93f16dce1604ec1d0
Dries Heyninck <dries.heyninck@gmail.com>
2017-04-04 18:09:52 +0200

    style updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,13 @@ import { HomePageComponent } from './components/home-page/home-page.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { NavBarProfileComponent } from './components/nav-bar/nav-bar-profile/nav-bar-profile.component';
 import { UrlSanitizerPipe } from './pipes/url-sanitizer.pipe';
+import { RegistrationPageComponent } from './components/registration-page/registration-page.component';
+import { RegistrationFormComponent } from './components/registration-page/registration-form/registration-form.component';
+import { BodyMovinBackgroundComponent } from './components/body-movin-background/body-movin-background.component';
+
+// import { StoreModule } from '@ngrx/store';
+
+import { APP_ROUTES } from './_routes/app.routes';
 
 @NgModule({
   declarations: [
@@ -15,12 +22,17 @@ import { UrlSanitizerPipe } from './pipes/url-sanitizer.pipe';
       HomePageComponent,
       NavBarComponent,
       NavBarProfileComponent,
-      UrlSanitizerPipe
+      UrlSanitizerPipe,
+      RegistrationPageComponent,
+      RegistrationFormComponent,
+      BodyMovinBackgroundComponent
   ],
   imports: [
       BrowserModule,
       FormsModule,
-      HttpModule
+      HttpModule,
+      APP_ROUTES
+      // StoreModule disabled for now.
   ],
   providers: [],
   bootstrap: [ AppComponent ]
