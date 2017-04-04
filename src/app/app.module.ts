f17085610fac05ff3c04891483dbc6d0968b4107
Dries Heyninck <dries.heyninck@gmail.com>
2017-04-04 17:51:12 +0200

    basic components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { NavBarProfileComponent } from './components/nav-bar/nav-bar-profile/nav-bar-profile.component';
+import { UrlSanitizerPipe } from './pipes/url-sanitizer.pipe';
 
 @NgModule({
   declarations: [
-      AppComponent
+      AppComponent,
+      HomePageComponent,
+      NavBarComponent,
+      NavBarProfileComponent,
+      UrlSanitizerPipe
   ],
   imports: [
       BrowserModule,
---
+++ src/app/components/nav-bar/nav-bar-profile/nav-bar-profile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-bar-profile',
+  templateUrl: './nav-bar-profile.component.html',
+  styleUrls: ['./nav-bar-profile.component.scss']
+})
+export class NavBarProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
