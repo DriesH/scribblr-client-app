bf9dcbcdf1bd49e1e94bafbf1ac9a8d63885b27e
Dries Heyninck <dries.heyninck@gmail.com>
2017-05-15 16:21:22 +0200

    setup error handler

--- src/app/services/error-handler.service.ts
+++ src/app/services/error-handler.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@angular/core';
 
+interface Error {
+    success: Boolean;
+    error_type: String;
+    error_message: String;
+    errors: Array<String>;
+    old_input: any;
+}
+
 @Injectable()
 export class ErrorHandlerService {
 
-  constructor() { }
+    constructor() { }
+
+    handler(error: Error) {
+
+    }
+
 
 }
