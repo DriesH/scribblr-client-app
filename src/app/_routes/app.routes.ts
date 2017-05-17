976d13ea14add3237a1fe0984c079e828d1ea657
Dries Heyninck <dries.heyninck@gmail.com>
2017-05-17 10:56:18 +0200

    hackermodus

--- src/app/_routes/app.routes.ts
+++ src/app/_routes/app.routes.ts
@@ -13,10 +13,6 @@ import { AchievementOverviewRootComponent } from '../components/application/appl
 import { BookOverviewRootComponent } from '../components/application/application-root/book-overview-root/book-overview-root.component';
 
 // Quote root component children
-// tslint:disable-next-line:max-line-length
-import { QuoteContainerComponent } from '../components/application/application-root/quote-overview-root/quote-container/quote-container.component';
-
-// Quote root component children
 import { ChildrenOverviewRootComponent } from '../components/application/application-root/children-overview-root/children-overview-root.component';
 import { NewChildComponent } from '../components/application/application-root/children-overview-root/new-child/new-child.component';
 
