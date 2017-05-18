2f0d397d5aee9267c311bae66395e8e19a3bfd7a
Dries Heyninck <dries.heyninck@gmail.com>
2017-05-18 14:40:42 +0200

    dat je een hoere bent

--- src/app/components/application/application-root/quote-overview-root/quote/quote.component.ts
+++ src/app/components/application/application-root/quote-overview-root/quote/quote.component.ts
@@ -18,27 +18,32 @@ export class QuoteComponent implements OnInit {
 
     imageHasBeenLoaded = false;
 
+    counter = 0;
+    scrollContainer: HTMLElement;
+
     constructor() { }
 
     ngOnInit() {
+        this.scrollContainer = document.getElementById('gridboi');
+
         if (this.last) {
             this.reachedLast.emit(true);
         }
     }
 
     formatImageSrc(child_short_id, quote_short_id, img_original_url_id?, img_baked_url_id?): String {
-        console.log('[formatImageSrc]: I got called.');
+        // console.log('[formatImageSrc]: I got called.');
 
-        console.log('[formatImageSrc]: child_short_id=', child_short_id);
-        console.log('[formatImageSrc]: quote_short_id=', quote_short_id);
-        console.log('[formatImageSrc]: img_original_url_id=', img_original_url_id);
-        console.log('[formatImageSrc]: img_baked_url_id=', img_baked_url_id);
+        // console.log('[formatImageSrc]: child_short_id=', child_short_id);
+        // console.log('[formatImageSrc]: quote_short_id=', quote_short_id);
+        // console.log('[formatImageSrc]: img_original_url_id=', img_original_url_id);
+        // console.log('[formatImageSrc]: img_baked_url_id=', img_baked_url_id);
 
         if (img_original_url_id) {
-            console.log('[formatImageSrc]: returned img_original_url_id.');
+            // console.log('[formatImageSrc]: returned img_original_url_id.');
             return API_ROUTES.baseUrl + API_ROUTES.application.quotes.imageOriginal(child_short_id, quote_short_id, img_original_url_id);
         } else if (img_baked_url_id) {
-            console.log('[formatImageSrc]: returned img_baked_url_id.');
+            // console.log('[formatImageSrc]: returned img_baked_url_id.');
             return API_ROUTES.baseUrl + API_ROUTES.application.quotes.imageBaked(child_short_id, quote_short_id, img_baked_url_id);
         } else {
             return null;
@@ -48,7 +53,9 @@ export class QuoteComponent implements OnInit {
 
     imageLoadedFn() {
         console.log('imageLoadedFn');
-        this.imageHasBeenLoaded = true;
+        setTimeout(() => {
+            this.imageHasBeenLoaded = true;
+        }, 400);
         this.imageLoaded.emit(true);
     }
 }
