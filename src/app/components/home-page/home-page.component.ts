fa7713bf81ade86cdf51026ac551e4dfcc008db7
Dries Heyninck <dries.heyninck@gmail.com>
2017-05-24 11:22:03 +0200

    update

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -7,17 +7,25 @@ import { Store } from '@ngrx/store';
 @Component({
     selector: 'scrblr-home-page',
     templateUrl: './home-page.component.html',
-    styleUrls: ['./home-page.component.scss']
+    styleUrls: ['./home-page.component.scss', './home.media.scss']
 })
 export class HomePageComponent implements OnInit {
+
+    isLoggedIn = false;
 
     constructor(private auth: AuthService, private store: Store<any>) { }
 
     ngOnInit() {
         this.store.select('CURRENT_USER').subscribe(CURRENT_USER => {
+            let currentUser: any = CURRENT_USER;
+
+            if (currentUser.isAuth) {
+                this.isLoggedIn = true;
+            }
+
             this.auth.getUser();
         });
     }
 
-    
+
 }
